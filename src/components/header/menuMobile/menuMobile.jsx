97c43c1882c7cc1446623ef51e1f9089acbcad6f
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2024-03-04 16:52:33 -0600

    add darkmode menu mobile

--- src/components/header/menuMobile/menuMobile.jsx
+++ src/components/header/menuMobile/menuMobile.jsx
@@ -2,14 +2,14 @@ import "./menuMobile.css";
 
 const MenuMobile = ({darkMode}) => {
   return (
-    <aside className="menuMobile">
-      <ol className="menuMobile__list">
+    <nav className={`menuMobile ${darkMode ? "menuMobile-dark": ""}`}>
+      <ol className={`menuMobile__list ${darkMode ? "menuMobile__list-dark": ""}`}>
         <li className="menuitem__list"><a href="#services">Servicios</a></li>
         <li className="menuitem__list"><a href="#services">Médicos</a></li>
         <li className="menuitem__list"><a href="#location">Ubicación</a></li>
         <li className="menuitem__list"><a href="#date">Agendar cita</a></li>
       </ol>
-    </aside>
+    </nav>
   );
 };
 
